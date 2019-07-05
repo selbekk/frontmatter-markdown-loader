6f069822dbfa1ca783039df35302f2c4aeec1871
hmsk <k.hamasaki@gmail.com>
2019-07-04 17:52:23 -0700

    Make loader bypassable

--- index.js
+++ index.js
@@ -19,33 +19,44 @@ module.exports = function (source) {
 
   const options = loaderUtils.getOptions(this) || {}
 
-  const fm = frontmatter(source)
-
-  if (options.markdown) {
-    fm.html = options.markdown(fm.body);
-  } else {
-    fm.html = md.render(fm.body);
-  }
-
-  const attributes = {
-    ...fm.attributes,
-    _meta: {
-      resourcePath: this.resourcePath
+  if (!options.bypassed) {
+    const fm = frontmatter(source)
+    if (options.markdown) {
+      fm.html = options.markdown(fm.body);
+    } else {
+      fm.html = md.render(fm.body);
     }
-  };
 
-  let output = `
-    body: ${stringify(fm.body)},
-    html: ${stringify(fm.html)},
-    attributes: ${stringify(attributes)}`;
+    const attributes = {
+      ...fm.attributes,
+      _meta: {
+        resourcePath: this.resourcePath,
+        query: this.query,
+        request: this.request,
+        resourceQuery: this.resourceQuery,
+        src: source
+      }
+    };
 
-  if (!!options.vue && vueCompiler && vueCompilerStripWith) {
-    const rootClass = options.vue.root || "frontmatter-markdown"
-    const template = fm
-      .html
+    const req = loaderUtils.stringifyRequest(this, `frontmatter-markdown-loader?bypassed!${this.resourcePath}`);
+    const vueRootClass = options.vue.root || "frontmatter-markdown";
+    const reqVue = loaderUtils.stringifyRequest(this, `frontmatter-markdown-loader?bypassed&compileVue&vueRootClass=${vueRootClass}!frontmatter-markdown-loader?bypassed!${this.resourcePath}`);
+    return `module.exports = {
+      "attributes": ${stringify(attributes)},
+      "html": require(${req}).default,
+      "body": ${stringify(fm.body)},
+      "originalHtml": ${stringify(fm.html)},
+      "vue": require(${reqVue}).default
+    };`;
+  } else if (options.compileVue) {
+    const html = JSON.parse(source.match(/^module.exports.default = (\".+\")$/)[1]);
+    // source should be HTML
+    // return Vue
+    // if (!!options.vue && vueCompiler && vueCompilerStripWith) {
+    const template = html
       .replace(/<(code\s.+)>/g, "<$1 v-pre>")
       .replace(/<code>/g, "<code v-pre>");
-    const compiled = vueCompiler.compile(`<div class="${rootClass}">${template}</div>`)
+    const compiled = vueCompiler.compile(`<div class="${options.vueRootClass}">${template}</div>`)
     const render = `return ${vueCompilerStripWith(`function render() { ${compiled.render} }`)}`
 
     let staticRenderFns = '';
@@ -53,27 +64,29 @@ module.exports = function (source) {
       staticRenderFns = `return ${vueCompilerStripWith(`[${compiled.staticRenderFns.map(fn => `function () { ${fn} }`).join(',')}]`)}`
     }
 
-    output += `,
-      vue: {
-        render: ${stringify(render)},
-        staticRenderFns: ${stringify(staticRenderFns)},
-        component: {
-          data: function () {
-            return {
-              templateRender: null
-            }
-          },
-          render: function (createElement) {
-            return this.templateRender ? this.templateRender() : createElement("div", "Rendering");
-          },
-          created: function () {
-            this.templateRender = ${vueCompilerStripWith(`function render() { ${compiled.render} }`)};
-            this.$options.staticRenderFns = ${vueCompilerStripWith(`[${compiled.staticRenderFns.map(fn => `function () { ${fn} }`).join(',')}]`)};
+    return `module.exports.default = {
+      render: ${stringify(render)},
+      staticRenderFns: ${stringify(staticRenderFns)},
+      component: {
+        data: function () {
+          return {
+            templateRender: null
           }
+        },
+        render: function (createElement) {
+          return this.templateRender ? this.templateRender() : createElement("div", "Rendering");
+        },
+        created: function () {
+          this.templateRender = ${vueCompilerStripWith(`function render() { ${compiled.render} }`)};
+          this.$options.staticRenderFns = ${vueCompilerStripWith(`[${compiled.staticRenderFns.map(fn => `function () { ${fn} }`).join(',')}]`)};
         }
       }
-    `;
+    }`;
+    //}
+  } else {
+    // source should be fm markdown
+    const originalHtml = source.match(/\"originalHtml\": (\".+\")/)[1];
+    // return HTML
+    return `module.exports.default = ${originalHtml}`;
   }
-
-  return `module.exports = { ${output} }`;
 }
